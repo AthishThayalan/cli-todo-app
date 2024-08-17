f9483f8e686155520781bc4efc74402915a89e05
Athish Thayalan <tathish@hotmail.co.uk>
2024-08-17 19:43:48 +0100

    FEATURE : Remove Task using a functional approach

--- src/main/scala/TodoApp.scala
+++ src/main/scala/TodoApp.scala
@@ -1,9 +1,13 @@
 
 object TodoApp {
-  def addTask(tasks:List[Task],description:String) : List[Task] = {
-    val newId:Int = if (tasks.isEmpty) 1 else tasks.maxBy(_.id).id +1 // if empty then 1 , else , do max missing num from id's
-    val newTask = Task(newId , description)
+  def addTask(tasks: List[Task], description: String): List[Task] = {
+    val newId: Int = if (tasks.isEmpty) 1 else tasks.maxBy(_.id).id + 1 // if empty then 1 , else , do max missing num from id's
+    val newTask = Task(newId, description)
 
     tasks :+ newTask
+  }
+
+  def removeTask(tasks:List[Task],id:Int): List[Task] = {
+    tasks.filterNot(_.id == id ) // if ids match then filter it out (remove it)
   }
 }
