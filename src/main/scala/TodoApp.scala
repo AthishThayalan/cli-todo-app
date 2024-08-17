dfe175688dc39f8ea1cc29226e46eb700b945954
Athish Thayalan <tathish@hotmail.co.uk>
2024-08-17 20:32:17 +0100

    Adjustment : Used 'Either' Monad for Error Handling

--- src/main/scala/TodoApp.scala
+++ src/main/scala/TodoApp.scala
@@ -1,29 +1,37 @@
 import scala.collection.IterableOnce.iterableOnceExtensionMethods
 
 object TodoApp {
-  def addTask(tasks: List[Task], description: String): List[Task] = {
-    val newId: Int = if (tasks.isEmpty) 1 else tasks.maxBy(_.id).id + 1 // if empty then 1 , else , do max missing num from id's
-    val newTask = Task(newId, description)
+  def addTask(tasks: List[Task], description: String): Either[String, List[Task]] = {
+    if (description.trim.isEmpty) Left("Task description cannot be empty.")
+    else {
+      val newId: Int = if (tasks.isEmpty) 1 else tasks.maxBy(_.id).id + 1 // if empty then 1 , else , do max missing num from id's
+      val newTask = Task(newId, description)
+      Right(tasks :+ newTask)
 
-    tasks :+ newTask
-  }
-
-  def removeTask(tasks: List[Task], id: Int): List[Task] = {
-    tasks.filterNot(_.id == id) // if ids match then filter it out (remove it)
-  }
-
-  def completeTask(tasks: List[Task], id: Int): List[Task] = {
-    tasks.map {
-      case task if task.id == id => task.copy(isCompleted = true)
-      case task => task
     }
-    // if id matches then replace with a copy (function way)
-    // else keep as is
+
+
+  }
+
+  def removeTask(tasks: List[Task], id: Int): Either[String, List[Task]] = {
+    if (tasks.exists(_.id == id)) Right(tasks.filterNot(_.id == id))
+    else Left(s"Task with ID: $id does not exist.")
+  }
+
+  def completeTask(tasks: List[Task], id: Int): Either[String, List[Task]] = {
+    if (tasks.exists(_.id == id)) {
+      Right(tasks.map {
+        case task if task.id == id => task.copy(isCompleted = true)
+        case task => task
+      })
+    }
+    else Left(s"Task with ID: $id does not exist.")
+
 
   }
 
   def listTasks(tasks: List[Task]): String = {
-    if(tasks.isEmpty){
+    if (tasks.isEmpty) {
       return "You have no current tasks ..."
     } else {
       tasks.map { task =>
